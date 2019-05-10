8f87284db626a781d883614cc271077c79bcef1e
levabala <levabala@ya.ru>
2019-05-10 15:30:42 +0300

    patch

--- major.sh
+++ major.sh
@@ -1,10 +1,10 @@
 node ./scripts/major.js
 
 message=${1:-updates}
-# target=${2:-master}
+target=${2:-master}
 # target=${2:-paralaxGrid}
 # target=${2:-fixedWidth}
-target=${2:-daysInteractionRedesign}
+# target=${2:-daysInteractionRedesign}
 
 git add ./
 git commit -m "$message"
--- patch.sh
+++ patch.sh
@@ -1,10 +1,10 @@
 node ./scripts/patch.js
 
 message=${1:-patch}
-# target=${2:-master}
+target=${2:-master}
 # target=${2:-paralaxGrid}
 # target=${2:-fixedWidth}
-target=${2:-daysInteractionRedesign}
+# target=${2:-daysInteractionRedesign}
 
 git add ./
 git commit -m "$message"
