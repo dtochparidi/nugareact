dbe037816a78c3527f143e9a9f64a6b56a12f5eb
levabala <levabala@ya.ru>
2019-06-11 17:27:07 +0300

    patch

--- src/components/CalendarCard/Day/index.ts
+++ src/components/CalendarCard/Day/index.ts
@@ -1,5 +1,4 @@
 export { default } from './Day';
 
-export { default as DateRow } from "./DateRow";
-export { default as GridP } from "./GridP";
-export { default as AppsBlock } from "./AppsBlock";
+export { default as DateRow } from './DateRow';
+export { default as GridP } from './GridP';
